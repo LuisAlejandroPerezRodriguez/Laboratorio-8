6f496411bb2874a671da7e2f164ba353725ced14
luis <luisalejandro.p.r@outlook.com>
2021-11-20 19:33:34 -0400

    Se agrego un menu

--- src/lab08/ExcepcionNegativa.java
+++ src/lab08/ExcepcionNegativa.java
@@ -1,14 +1,9 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+    Estudiante: Luis Alejandro Perez Rodriguez
+    Matricula: 10141930
  */
 package lab08;
 
-/**
- *
- * @author luisa
- */
 public class ExcepcionNegativa extends IllegalArgumentException {
     
     //Excepcion cuando el numero es negativo
--- src/lab08/Lab08.java
+++ src/lab08/Lab08.java
@@ -3,14 +3,30 @@
     Matricula: 10141930
  */
 package lab08;
+import java.util.Scanner;
 
 public class Lab08 {
 
     public static void main(String[] args) {
+         Scanner key1=new Scanner(System.in);
+         int respuesta; 
+         char desicion='y';
+         
+         for(;desicion!='n';){ 
+       do{
+        System.out.println(" Introduzca #1. para ver proramming Challenges 1"
+                + "\n Introduzca #2. para ver proramming Challenges 2"+ "\n Introduzca #3. para ver proramming Challenges 9" 
+                +"\n Introduzca #0. para salir del programa");
+        respuesta=key1.nextInt();
+       } while( respuesta!=0 && respuesta>4);
+       
+        if(respuesta!=0 ){
+       switch (respuesta){
         
+           case 1:
         //problema 1
         try {
-            int[] test={99,-88,102,77};
+            int[] test={99,-88,102,77}; // ← OJO aqui tiene que poner un numero negativo o mayor de 100 para probar las excepciones
             TestScores TS1=new TestScores(test);
             
         }catch(ExcepcionNegativa Error1){
@@ -18,8 +34,13 @@ public class Lab08 {
         }catch(ExcepcionMasDeCien Error2){
             System.out.println(Error2.getMessage());
         }
-            
-        
+         break;
+         
+       }
+        }
+        System.out.println("Desea continuar? (Escribir y para continuar o n para salir del programa)");
+        desicion=key1.next().charAt(0); 
+         }
     
     }
 
